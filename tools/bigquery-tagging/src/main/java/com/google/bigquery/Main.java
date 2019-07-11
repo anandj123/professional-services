8fe39f88d0b145ccb1e9fa569a382e00baf06736
Anand Jain <jain.12@gmail.com>
2019-07-11 13:52:30 -0500

    Test added

--- tools/bigquery-tagging/src/main/java/com/google/bigquery/Main.java
+++ tools/bigquery-tagging/src/main/java/com/google/bigquery/Main.java
@@ -1,6 +1,10 @@
 package com.google.bigquery;
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!!");
+    System.out.println(helloworld());
+  }
+
+  public static String helloworld(){
+    return "Hello World!!";
   }
 }
---
+++ tools/bigquery-tagging/src/test/java/com/google/bigquery/TestMain.java
@@ -0,0 +1,14 @@
+package com.google.bigquery;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMain {
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Hello World!!", Main.helloworld());
+    }
+
+}
