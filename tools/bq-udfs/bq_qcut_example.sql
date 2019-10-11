f43b6b6a215d8b6e8e5c2963d6cc13725e4df14a
Anand Jain <jain.12@gmail.com>
2019-10-11 15:29:40 -0400

    commited on Fri Oct 11 15:29:40 EDT 2019

--- tools/bq-udfs/bq_qcut_example.sql
+++ tools/bq-udfs/bq_qcut_example.sql
@@ -1,39 +1,48 @@
-#--------------------------------------------------------------
-# User defined function to do calculation like pandas qcut
-# the ranking is 0 based index e.g. 20 buckets starts from 0
-# and runs till 19
-#--------------------------------------------------------------
-CREATE TEMP FUNCTION qcut(buckets INT64, countA INT64, val FLOAT64) RETURNS INT64 AS 
-(
-  cast(floor(val * (buckets / countA) - (buckets / countA)) as int64)
-);
-
-#--------------------------------------------------------------
-# Analytics function rank() over() throws out of memory errors when ran
-# on a large dataset. So here the total dataset is divided into
-# 200 buckets and each one is ranked individually. Then they are
-# combined by adding the sum of the records in all the previous
-# buckets.
-#--------------------------------------------------------------
-
-select 
-  HH_ID
-  , Scores
-  ,qcut(20, 
-        (select count(*) from `anand-bq-test-2.Anand_BQ_Test_1.scores100m`), 
-        rank() over(partition by cast(floor(Scores * 200) as int64) order by Scores) + b.rank_part)  qcut
-from `anand-bq-test-2.Anand_BQ_Test_1.scores100m` a
-inner join 
-(
-  select 
-    part
-    , sum(count_part) over (order by part) - count_part rank_part
-  from (
-          select 
-              cast(floor(Scores * 200) as int64) part
-              ,count(Scores) count_part
-          from `anand-bq-test-2.Anand_BQ_Test_1.scores100m`
-          group by cast(floor(Scores * 200) as int64)
-       )
- ) b
-ON cast(floor(Scores * 200) as int64) = b.part
+DECLARE
+  N_BUCKETS,
+  N_PARTITIONS,
+  N_COUNT INT64;
+SET
+  N_BUCKETS = 20;
+SET
+  N_PARTITIONS = 200;
+SET
+  N_COUNT = (
+  SELECT
+    COUNT(*)
+  FROM
+    `anand-bq-test-2.Anand_BQ_Test_1.scores100m`);
+  #--------------------------------------------------------------
+  # User defined function to do calculation like pandas qcut
+  # the ranking is 0 based index e.g. 20 buckets starts from 0
+  # and runs till 19
+  #--------------------------------------------------------------
+CREATE TEMP FUNCTION
+  qcut(buckets INT64,
+    countA INT64,
+    rank FLOAT64)
+  RETURNS INT64 AS ( CAST(FLOOR(rank * (buckets / countA) - (buckets / countA)) AS int64) );
+SELECT
+  HH_ID,
+  Scores,
+  qcut(N_BUCKETS,
+    N_COUNT,
+    RANK() OVER(PARTITION BY CAST(FLOOR(Scores * 200) AS int64)
+    ORDER BY
+      Scores) + b.rank_part) qcut
+FROM
+  `anand-bq-test-2.Anand_BQ_Test_1.scores100m` a
+INNER JOIN (
+  SELECT
+    part,
+    SUM(count_part) OVER (ORDER BY part) - count_part rank_part
+  FROM (
+    SELECT
+      CAST(FLOOR(Scores * 200) AS INT64) part,
+      COUNT(Scores) count_part
+    FROM
+      `anand-bq-test-2.Anand_BQ_Test_1.scores100m`
+    GROUP BY
+      CAST(FLOOR(Scores * 200) AS INT64) ) ) b
+ON
+  CAST(FLOOR(Scores * 200) AS INT64) = b.part
